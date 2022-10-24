800737a68ef02957b222c38ee64e5b25956c24cb
Ben Clayton <ben@calvium.com>
2022-10-24 18:06:52 +0100

    obs

--- newrace.lua
+++ newrace.lua
@@ -1,4 +1,4 @@
-col={
+local col={
     green1=3,
     green2=11,
     blue1=1,
@@ -11,15 +11,15 @@ col={
     red2=8
 }
 
-distance=0
-speed=0
-maxspeed=3
-x=0
-dx=0
-steerspeed=0.4
-skid=false
+local distance=0
+local speed=0
+local maxspeed=3
+local x=0
+local dx=0
+local steerspeed=0.4
+local skid=false
 
-carsprite={
+local carsprite={
     center=64,
     l1=196,
     l2=200,
@@ -68,6 +68,27 @@ function _update60()
     dx=dx-throwspeed
 
     x=x+dx
+    local off=60
+    if x<-off or x>off then 
+        if x<-off then x=-off end
+        if x>off then x=off end
+        if (speed>(maxspeed/4)) then
+            speed*=0.95
+        end
+        if rumble==false then
+            rumble=true
+            -- TODO the sound effect isn't working yet
+            -- sfx(1,1)
+        end
+    else
+        -- if rumble==true then
+        --     rumble=false
+        --     sfx(-1,1)
+        -- end
+    end
+    
+
+
     distance=(distance+speed)%(tracklength*100)
 
     local trackidx=flr(distance/100)
@@ -82,32 +103,7 @@ function _update60()
     bgx=(bgx-turn*(speed/maxspeed)*0.75)%128
 end
 
-shadingrdtx = {
-    -- 0,
-    -- 2,
-    -- 4,
-    6,
-    8,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-    12,
-}
-
-shadinggrs = {
+local shadinggrs = {
     {col.green1,col.grey1},
     {col.green1,col.green2},
     {col.green1,col.green2},
@@ -121,11 +117,23 @@ shadinggrs = {
     {col.green1,col.green2},
 }
 
+local shadingroad = {
+    col.blue2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+    col.grey2,
+}
+
 shadinglns = {
-    {col.grey1,col.red1},
-    {col.blue2,col.red1},
-    -- {col.grey2,col.red2},
-    -- {col.grey2,col.red2},
+    {col.grey2,col.red1},
+    {col.white,col.red1},
     {col.white,col.red2},
     {col.white,col.red2},
     {col.white,col.red2},
@@ -144,7 +152,7 @@ height=0
 
 track={
     0,
-    0.3,1,1,1,1,0.3,
+    0.3,0.7,0.7,0.7,0.7,0.3,
     0,
     -0.3,-1,-1,-1,-0.3,
     0,
@@ -157,132 +165,73 @@ tracklength=count(track)
 bgx=0
 screensize=128
 
--- converts x -1..1 to a position on the road in pixels
--- much %age down road (0=top)
--- midw middle of the screen
--- roadw2 width of the road
-function roadX(x, roadw2, much)
-    local perspective = 0.8*(much)+0.08
-    local roadw=roadw2*0.5*perspective
-    local midw=0.5+(turn*(1-much))^3
-    local x2 = midw-(x*perspective*screensize)--midw-roadw-x
-    return x2
-end
+-- -- converts x -1..1 to a position on the road in pixels
+-- -- much %age down road (0=top)
+-- -- midw middle of the screen
+-- -- roadw2 width of the road
+-- function roadX(x, roadw2, much)
+--     local perspective = 0.8*(much)+0.08
+--     local roadw=roadw2*0.5*perspective
+--     local midw=0.5+(turn*(1-much))^3
+--     local x2 = midw-(x*perspective*screensize)--midw-roadw-x
+--     return x2
+-- end
 
 -------------------
 -- roadside objects
 
-local obs = {
-    -- {
-    --     d=15,
-    --     x=0.5,
-    --     spr=1,
-    --     hw=1,
-    --     sh=1,
-    -- },
-    -- {
-    --     d=20,
-    --     x=-0.5,
-    --     spr=1,
-    --     hw=1,
-    --     sh=1,
-    -- },
-    -- {
-    --     d=25,
-    --     x=0.5,
-    --     spr=1,
-    --     hw=1,
-    --     sh=1,
-    -- },
-    -- {
-    --     d=30,
-    --     x=0.5,
-    --     spr=1,
-    --     hw=1,
-    --     sh=1,
-    -- },
-    -- {
-    --     d=35,
-    --     x=-0.5,
-    --     spr=1,
-    --     hw=1,
-    --     sh=1,
-    -- }
-}
-
-obtypes={
-    tree={
-        sprx=4*8, -- spr 1 coords
-        spry=0,
-        w=3,
-        h=4,
-        ax=1, -- anchor right
-    },
-    sign={
-        sprx=7*8, -- spr 1 coords
-        spry=0,
-        w=3,
-        h=2,
-        ax=1, -- anchor right
-    }
-}
+local obs = {}
 
 -- temp gen obs
-for i=0,100 do
-    local pos={
-        d=i*25,
-        x=-0.5,
-    }
-
+for i=0,200 do
+    local ob
     local obchoice=flr(rnd(3))
     if obchoice==0 then
-        add(obs, {
-            d=i*25,
-        x=-0.5,
+        -- TREE
+        ob={
             sprx=4*8, -- spr 1 coords
             spry=0,
             w=3,
             h=4,
-            ax=1, -- anchor right
-        })
-    else if obchoice==1 then
-        add(obs, {
-            d=i*25,
-            x=-0.5,
-            sprx=7*8, -- spr 1 coords
+            ax=1+rnd(1)/5, -- anchor right
+            flipx=flr(rnd(2))==0,
+        }
+    elseif obchoice==1 then
+        -- Lard sign
+        ob={
+            sprx=7*8,
             spry=0,
             w=3,
             h=2,
             ax=1, -- anchor right
-        })
+        }
     else
-        add(obs, {
-            d=i*25,
-            x=-0.5,
-            sprx=7*8, -- spr 1 coords
+        -- 30 sign
+        ob={
+            sprx=7*8, 
             spry=2*8,
             w=1,
             h=2,
             ax=2, -- anchor right
-        })
+        }
     end
+
+    ob.d=i*10
+    local isLeft=flr(rnd(2))==0;
+    ob.x=isLeft and -1 or 1
+    ob.scale=3
+    if not isLeft then
+        ob.ax = -ob.ax
+        -- flip anchor
+    end
+    add(obs, ob, 1)
 end
-    -- add(obs, {
-    --     d=i*10,
-    --     x=0.5,
-    --     sprx=5*8,
-    --     spry=0,
-    --     w=4,
-    --     h=4,
-    --     ax=-1, -- anchor left
-    -- })
-end
+
+local roadh=78+height
 
 function _draw()
     cls()
     
-    local roadh=78+height
-
     -- background
     map(0,5,bgx-128,64-8-roadh,16,16)
     map(0,5,bgx,64-8-roadh,16,16)
@@ -308,7 +257,7 @@ function _draw()
         
         local isStripe = sin(10*((1-perspective)^3)+(distance*0.05)%3.14)>0
         
-        local shade=shadingrdtx[flr(much*16)+1]
+        -- local shade=shadingrdtx[flr(much*16)+1]
         --local roady = l and shade or shade+2
 
         local shade2=shadinggrs[flr(much*10)+1]
@@ -318,11 +267,14 @@ function _draw()
         local linecolor = isStripe and shade2[1] or shade2[2]
 
         -- draw road first over whole screen
-        local tlineY = (shade+(y%2))/8
-        if isStripe then
-         tlineY=tlineY+2
-        end
-        tline(0, y, 128, y, 0, tlineY)
+        -- local tlineY = (shade+(y%2))/8
+        -- if isStripe then
+        --  tlineY=tlineY+2
+        -- end
+        -- tline(0, y, 128, y, 0, tlineY)
+
+        local roadcolor=shadingroad[flr(much*10)+1]
+        line(ledge, y, redge, y, roadcolor)
         line(0, y, lgrassw, y, grasscolor)
 
         -- left edge
@@ -330,6 +282,11 @@ function _draw()
         
         line(redge,y,rgrass, y, linecolor)
         line(rgrass, y, screensize, y, grasscolor)
+
+        -- line in center of road
+        if isStripe then
+            line((midw-roadw*0.035)*screensize, y, (midw+roadw*0.035)*screensize, y, linecolor)
+        end
 
     end
 
@@ -347,6 +304,44 @@ function _draw()
         end
     end
 
+    color(6)
+    print(stat(7))
+    print("distance: "..flr(distance))
+    print("x: "..x)
+    --print("track: "..getTrack(distance) and getTrack(distance) or -1)
+    -- print(turn)
+    
+
+    -- DRAW ROADSIDE OBJECTS
+    for ob in all(obs) do
+        drawobject(ob)
+    end
+
+    -- DRAW ENEMY CARS
+    -- temp
+    drawobject({
+        d=50,
+        x=-.3,
+        sprx=0*8, 
+        spry=1*8,
+        w=4,
+        h=3,
+        ax=0.5, -- anchor center
+        scale=1.25
+    })
+
+    drawobject({
+        d=100,
+        x=.3,
+        sprx=0*8, 
+        spry=1*8,
+        w=4,
+        h=3,
+        ax=0.5, -- anchor center
+        scale=1.25
+    })
+
+    -- DRAW THE CAR
     local tr=0 --btn(1) and 1 or 0 --//turn>0.5 and 1 or 0
     local tl=0 --btn(0) and 1 or 0-- turn<-0.5 and 1 or 0
     spr(carspr,
@@ -359,89 +354,38 @@ function _draw()
     16+64-16+turn*16+x,
     96+height/10+tr,
     2,3)
+end
 
-    color(6)
-    print(stat(7))
-    print("distance: "..flr(distance))
-    print("x: "..x)
-    --print("track: "..getTrack(distance) and getTrack(distance) or -1)
-    print(turn)
-    
 
-    -- temp object
-    for ob in all(obs) do
-        local obd=ob.d-distance
-        local clipd=100
-        if obd > 0 and obd<clipd then
-            local ay=((clipd-obd)/clipd)^3.5
-            -- not quite right speed
-            
-            local perspective = 0.8*(ay)+0.08
-            local midw=0.5+(turn*(1-ay))^3
-            local roadw=1.2
-            roadw=roadw*0.5*perspective
-            local px=(midw-roadw)*screensize
-            
-            printh("px:"..px.." ob.x:"..ob.x.." midw:"..midw.." ob.x+midw:"..ob.x+midw)
+function drawobject(ob) 
+    local obd=ob.d-distance
+    local clipd=100
+    if obd > -10 and obd<clipd then
+        local ay=((clipd-obd)/clipd)^3.5
+        -- not quite right speed
+        -- also duplicated from road drawing, move into function
+        local perspective = 0.8*(ay)+0.08
+        local midw=0.5+(turn*(1-ay))^3
+        local roadw=1.2
+        roadw=roadw*0.5*perspective
+        local px=(midw+roadw*ob.x)*screensize
+        
+        printh("px:"..px.." ob.x:"..ob.x.." midw:"..midw.." ob.x+midw:"..ob.x+midw)
 
-            local py=ay*roadh+(screensize-roadh)
-            --sspr takes a RECTANGLE from the sprite sheet
-            local scaledh=ob.h*8*perspective*3
-            local scaledw=ob.w*8*perspective*3
-            sspr(
-                ob.sprx,
-                ob.spry,
-                ob.w*8,
-                ob.h*8,
-                px-scaledw*ob.ax,
-                py-scaledh, --move up based on the height
-                scaledw,
-                scaledh
-            )
-
-                
-            -- px,
-            --  ay*roadh+(screensize-roadh)-6, 
-            --  1, 1)
-        end
-        -- spr(1, 50, 50, 1, 1)
+        local py=ay*roadh+(screensize-roadh)
+        --sspr takes a RECTANGLE from the sprite sheet, not a sprite number (for some reason)
+        local scaledh=ob.h*8*perspective*ob.scale
+        local scaledw=ob.w*8*perspective*ob.scale
+        sspr(
+            ob.sprx,
+            ob.spry,
+            ob.w*8,
+            ob.h*8,
+            px-scaledw*ob.ax,
+            py-scaledh, --move up based on the height
+            scaledw,
+            scaledh,
+            ob.flipx
+        )
     end
 end
-
--- -- temp
--- local screenCenter = {screensize.x/2, screensize.y/2}; // half of your screen resolution
---                                                             // try changing it to see what happens
---                                                             // you can animate it to add special effects
--- float perspStrength = 500.0f;   // actually an inverse of perspective strength
---                                 // lower values means bigger effect
-
--- Vec2 project( Vec3 pos )        // projection code
--- {
---     Vec2 localPos = pos - screenCenter; // Move point (0,0) to the center of your screen
-
---     float scale = pos.z/perspStrength;  // scale your Z to ~(0-1) range
---     scale += 1.0f;                      // move your Z to ~(1-2) range
-
---     localPos.x /= scale;                // scale position according to Z distance
---     localPos.y /= scale;
-
---     Vec2 result = localPos + screenCenter;  // Move point (0,0) back to the corner
-
---     return result;
--- //    return Vec3( result.x, result.y, scale ); // you can also return scale used for this object
---                                                 // so you can render distant objects as smaller
--- }
--- --end
-
-
-function updateOb() 
-end
-
-function drawOb()
-end
-
--- 3d Projection
--- y_screen = (y_world*scale / z) + (screen_height >> 1)
--- or:
--- z = (y_world*scale) / (y_screen - (screen_height >> 1))
--- http://www.extentofthejam.com/pseudo/#curves
